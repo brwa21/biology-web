7cdf9f3c46166702f6657621cd1f718519808ac9
brwa21 <notebrwa@gmail.com>
2026-01-20 18:06:16 +0300

    Refactor app.py for clarity and functionality

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ instruction = (
 )
 # بەکارهێنانی مۆدێلی چاککراو
 model = genai.GenerativeModel(
-    model_name='gemini-1.5-flash'' # وشەی latest دڵنیایی دەدات کە کۆن نییە
+    model_name='gemini-1.5-flash' # وشەی latest دڵنیایی دەدات کە کۆن نییە
 )
 
 
@@ -40,4 +40,5 @@ if user_query:
         st.write("---")
 
         st.markdown(response.text)
+
 
